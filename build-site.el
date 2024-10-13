0e114218bacbee4129c5df004a2cde04e9cd5832
likithjai <jailikith@gmail.com>
2024-10-13 22:54:50 +0530

    first stab at making a website using emacs org mode

---
+++ build-site.el
@@ -0,0 +1,14 @@
+;; load the publishing system
+(require 'ox-publish)
+
+;; define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "drlikith-website"
+	     :recursive t
+	     :base-directory "./content"
+	     :publishing-directory "./public"
+	     :publishing-function 'org-html-publish-to-html)))
+(org-publish-all t)
+
+(message "build complete!")
