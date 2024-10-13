1f6758d09a268710b419e377bd83c9d6b62f1b73
likithjai <jailikith@gmail.com>
2024-10-13 23:50:44 +0530

    added some styling elements for code blocks, and added emacs config page (as practice for adding link functionality)

--- build-site.el
+++ build-site.el
@@ -1,5 +1,27 @@
+;; set the package installation directory so that packages aren't stored in the ~/.emacs.d/elpa path
+
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; intialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install dependencies
+(package-install 'htmlize)
+
+
+
 ;; load the publishing system
 (require 'ox-publish)
+
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; define the publishing project
 (setq org-publish-project-alist
@@ -11,7 +33,7 @@
 	     :publishing-function 'org-html-publish-to-html
 	     :with-author nil ;; doesn't include author name
 	     :with-creator t ;;include emacs and org versions in footer
-	     :with-toc t ;;include a table of contents
+	     :with-toc nil ;;include a table of contents
 	     :section-numbers nil ;; don't include section numbers
 	     :time-stamp-file nil))) ;; don't include timestamp in file
 (org-publish-all t)
