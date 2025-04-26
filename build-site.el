5a7be9ab41f932eb089c4e413bda3342588ecaed
likithjai <jailikith@gmail.com>
2025-04-26 15:32:12 +0530

    improving the blog page functionality

--- build-site.el
+++ build-site.el
@@ -18,30 +18,84 @@
 ;; load the publishing system
 (require 'ox-publish)
 
+(require 'org)
+(require 'ob-core)
+(require 'ob-lisp)
+
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (shell . t)
+   (python . t)))
+
+(setq org-confirm-babel-evaluate
+	  (lambda (lang body)
+		(not (member lang '("emacs-lisp" "python")))))
+
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" href=\"./styles/styles.css\" />")
 
 ;; define the publishing project
+;; (setq org-publish-project-alist
+;;       (list
+;;        (list "drlikith-website"
+;; 	     :recursive t
+;; 		 :base-url "https://likithjai.github.io/drlikith-website/"
+;; 	     :base-directory "./content"
+;; 	     :publishing-directory "."
+;; 	     :publishing-function 'org-html-publish-to-html
+;; 	     :with-author nil ;; doesn't include author name
+;; 	     :with-creator nil ;;include emacs and org versions in footer
+;; 	     :with-toc nil ;;include a table of contents
+;; 	     :section-numbers nil ;; don't include section numbers
+;; 	     :time-stamp-file nil))) ;; don't include timestamp in file
+;; Required for org-babel execution
 (setq org-publish-project-alist
-      (list
-       (list "drlikith-website"
-	     :recursive t
-		 :base-url "https://likithjai.github.io/drlikith-website/"
-	     :base-directory "./content"
-	     :publishing-directory "."
-	     :publishing-function 'org-html-publish-to-html
-	     :with-author nil ;; doesn't include author name
-	     :with-creator nil ;;include emacs and org versions in footer
-	     :with-toc nil ;;include a table of contents
-	     :section-numbers nil ;; don't include section numbers
-	     :time-stamp-file nil))) ;; don't include timestamp in file
+      `(("site-content"
+         :base-directory "./content"
+         :base-extension "org"
+         :publishing-directory "."
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :auto-preamble t
+         :auto-sitemap nil  ; We'll handle the blog index separately
+         :htmlized-source t
+         :html-head ,org-html-head)
+        
+        ("site-blog-posts"
+         :base-directory "./content/blog"
+         :base-extension "org"
+         :publishing-directory "."
+         :recursive nil
+         :publishing-function org-html-publish-to-html
+         :section-numbers nil
+         :with-toc nil
+         :htmlized-source t)
+        
+        ("site-blog-index"
+         :base-directory "./content"
+         :base-extension "org"
+         :publishing-directory "."
+         :recursive nil
+         :publishing-function org-html-publish-to-html
+         :auto-sitemap nil
+         :htmlized-source t)
+        
+        ("site" :components ("site-content" "site-blog-posts" "site-blog-index"))))
+
+
 (setq org-html-validation-link nil ;; don't show validation link
 	  org-html-head-include-scripts nil ;; use our own scripts
 	  org-html-head-include-default-style nil ;; use our own styles
 	  org-html-head "<link rel=\"stylesheet\" href=\"./styles/styles.css\" />")
+
+;; Enable essential org features
 (org-publish-all t)
+
 
 
 (message "build complete!")
