593263af5aaad8e61ad1a7f5e0f8720919c455e9
likithjai <jailikith@gmail.com>
2025-05-11 20:24:19 +0530

    fixing post1 not being published

--- build-site.el
+++ build-site.el
@@ -30,7 +30,6 @@
   (org-html-export-to-html))
 
 
-(update-blog-index)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
@@ -48,21 +47,6 @@
       org-export-with-section-numbers nil
       org-html-head "<link rel=\"stylesheet\" href=\"/styles/styles.css\" />")
 
-;; define the publishing project
-;; (setq org-publish-project-alist
-;;       (list
-;;        (list "drlikith-website"
-;; 	     :recursive t
-;; 		 :base-url "https://likithjai.github.io/drlikith-website/"
-;; 	     :base-directory "./content"
-;; 	     :publishing-directory "."
-;; 	     :publishing-function 'org-html-publish-to-html
-;; 	     :with-author nil ;; doesn't include author name
-;; 	     :with-creator nil ;;include emacs and org versions in footer
-;; 	     :with-toc nil ;;include a table of contents
-;; 	     :section-numbers nil ;; don't include section numbers
-;; 	     :time-stamp-file nil))) ;; don't include timestamp in file
-;; Required for org-babel execution
 (setq org-publish-project-alist
       `(("site-content"
          :base-directory "./content"
@@ -78,17 +62,17 @@
 		 :with-toc nil
          :html-head ,org-html-head)
         
-        ("site-blog-posts"
-         :base-directory "./content/blog"
-         :base-extension "org"
-         :publishing-directory "."
-         :recursive nil
-         :publishing-function org-html-publish-to-html
-         :section-numbers nil
-         :with-toc nil
-		 :html-postamble nil
-		 :html-head ,org-html-head
-         :htmlized-source t)
+        ;; ("site-blog-posts"
+        ;;  :base-directory "./content/blog/posts"
+        ;;  :base-extension "org"
+        ;;  :publishing-directory "."
+        ;;  :recursive nil
+        ;;  :publishing-function org-html-publish-to-html
+        ;;  :section-numbers nil
+        ;;  :with-toc nil
+		;;  :html-postamble nil
+		;;  :html-head ,org-html-head
+        ;;  :htmlized-source t)
         
         ("site-blog-index"
          :base-directory "./content"
@@ -103,17 +87,14 @@
 		 :html-postamble nil
          :htmlized-source t)
         
-        ("site" :components ("site-content" "site-blog-posts" "site-blog-index"))))
+        ("site" :components ("site-content" "site-blog-index"))))
 
 
-;; (setq org-html-validation-link nil ;; don't show validation link
-;; 	  org-html-head-include-scripts nil ;; use our own scripts
-;; 	  org-html-head-include-default-style nil ;; use our own styles
-;; 	  org-html-head "<link rel=\"stylesheet\" href=\"./styles/styles.css\" />")
 
 ;; Enable essential org features
 
 (org-publish-all t)
 
+(update-blog-index)
 
 (message "build complete!")
