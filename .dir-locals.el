38c027726e82359dd7a1a94536c174d5638ef3ba
likith <jailikith@gmail.com>
2025-09-21 19:58:56 +0530

    starting from scratch again

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-((org-mode . ((eval . (add-hook 'after-save-hook
-                                 (lambda ()
-                                   (when (and buffer-file-name
-                                              (string= (file-name-extension buffer-file-name) "org"))
-                                     (org-md-export-to-markdown)))
-                                 nil
-                                 'local))))
--- content/_index.md
+++
@@ -1,8 +0,0 @@
-+++
-[extra]
-land = 'en'
-+++
-
-# welcome
-
-I hope to share things that are interesting and on my mind here. hope you are cozy.
